463f0b50234b9d5b0b48deab3dd8f0f21107f33e
Krish Jani <65438720+KrishJani@users.noreply.github.com>
2022-07-27 11:48:42 +0530

    Add files via upload

---
+++ SkillTracker-Java-master/src/main/java/com/tracker/model/AssociateSkills.java
@@ -0,0 +1,22 @@
+package com.tracker.model;
+
+import java.io.Serializable;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AssociateSkills.
+ */
+public class AssociateSkills implements Serializable{
+
+    /** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The associate id. */
+	int associateId;
+	
+	/** The skill id. */
+	int skillId;
+	
+	
+	
+}
